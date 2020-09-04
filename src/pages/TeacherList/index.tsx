17616e64012bf3a9aa14d41311513db66f27e813
Apolinario-Manuel <66335189+Apolinario-Manuel@users.noreply.github.com>
2020-09-04 10:12:28 +0100

    Update index.tsx

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -60,11 +60,11 @@ function TeacherList() {
             options={[
               { value: "0", label: "Domingo" },
               { value: "1", label: "Segunda-feira" },
-              { value: "2", label: "Tersa-feira" },
+              { value: "2", label: "Terça-feira" },
               { value: "3", label: "Quarta-feira" },
               { value: "4 fisica", label: "Quinta-feira" },
               { value: "5", label: "Sexta-feira" },
-              { value: "6", label: "Sabado" },
+              { value: "6", label: "Sábado" },
             ]}
           />
           <Input 
